0d6053f0fd8603364c684512402c6a59da28733e
proalamin-247 <pro.alamin247@gmail.com>
2022-05-23 12:56:41 +0600

    node server connect mongoDB seccesfull

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
@@ -6,6 +7,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvzcx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    console.log('db connected')
+});
 
 app.get('/', (req, res) => {
     res.send('manufacturer-website server')
