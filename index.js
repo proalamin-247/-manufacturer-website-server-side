be25bd37854721ac88e5f84ef5dcba620a778523
proalamin-247 <pro.alamin247@gmail.com>
2022-05-23 15:21:55 +0600

    get method added for display database all parts in ui

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 app.use(cors());
 app.use(express.json());
@@ -12,12 +12,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvzcx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-    console.log('db connected')
-});
+async function run() {
+    try {
+        await client.connect();
+        const partCollection = client.db('manufacturer').collection('parts')
+
+        app.get('/part', async(req, res)=>{
+            const query={};
+            const cursor = partCollection.find(query);
+            const parts= await cursor.toArray();
+            res.send(parts);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('manufacturer-website server')
