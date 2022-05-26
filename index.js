f2e9d410396c4f96564a8f5d072942659f6fb457
proalamin-247 <pro.alamin247@gmail.com>
2022-05-26 13:10:44 +0600

    create afunction for sucure api

--- index.js
+++ index.js
@@ -9,9 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvzcx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 
 async function run() {
     try {
@@ -78,15 +93,18 @@ async function run() {
         })
 
         // get order deatils
-        app.get('/order', async (req, res) => {
+        app.get('/order',verifyJWT, async (req, res) => {
             const userEmail = req.query.userEmail;
-            const query = { userEmail: userEmail };
-            const orders = await orderCollection.find(query).toArray();
-            res.send(orders);
-            // const query = {};
-            // const cursor = orderCollection.find(query);
-            // const orders = await cursor.toArray();
-            // res.send(orders);
+            const decodedEmail = req.decoded.email;
+            if (userEmail===decodedEmail){
+                const query = { userEmail: userEmail };
+                const orders = await orderCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else{
+                return res.status(403).send({message: 'forbidden access'});
+            }
+            
         })
 
 
